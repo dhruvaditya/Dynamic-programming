d167558bb1cfb08d9bd61ae810c42a23d95b49fe
Aditya Raj <89244720+dhruvaditya@users.noreply.github.com>
2022-11-09 23:03:53 +0530

    Update code.cpp

--- Minimum_Sub Set Difference/code.cpp
+++ Minimum_Sub Set Difference/code.cpp
@@ -1,42 +1,81 @@
-// A recursive solution for subset sum problem
-#include <iostream>
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
 using namespace std;
 
-// Returns true if there is a subset
-// of set[] with sum equal to given sum
-bool isSubsetSum(int set[], int n, int sum)
+// } Driver Code Ends
+class Solution{
+
+  public:
+	int minDifference(int arr[], int n)  { 
+	     int sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += arr[i];
+ 
+    // Create an array to store results of subproblems
+    bool dp[n + 1][sum + 1];
+ 
+    // Initialize first column as true. 0 sum is possible
+    // with all elements.
+    for (int i = 0; i <= n; i++)
+        dp[i][0] = true;
+ 
+    // Initialize top row, except dp[0][0], as false. With
+    // 0 elements, no other sum except 0 is possible
+    for (int i = 1; i <= sum; i++)
+        dp[0][i] = false;
+ 
+    // Fill the partition table in bottom up manner
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= sum; j++) {
+            // If i'th element is excluded
+            dp[i][j] = dp[i - 1][j];
+ 
+            // If i'th element is included
+            if (arr[i - 1] <= j)
+                dp[i][j] |= dp[i - 1][j - arr[i - 1]];
+        }
+    }
+ 
+    // Initialize difference of two sums.
+    int diff = INT_MAX;
+ 
+    // Find the largest j such that dp[n][j]
+    // is true where j loops from sum/2 t0 0
+    for (int j = sum / 2; j >= 0; j--) {
+        // Find the
+        if (dp[n][j] == true) {
+            diff = sum - 2 * j;
+            break;
+        }
+    }
+    return diff;
+	}
+};
+
+
+//{ Driver Code Starts.
+int main() 
 {
-	
-	// Base Cases
-	if (sum == 0)
-		return true;
-	if (n == 0)
-		return false;
+   
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
 
-	// If last element is greater than sum,
-	// then ignore it
-	if (set[n - 1] > sum)
-		return isSubsetSum(set, n - 1, sum);
+        int a[n];
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
 
-	/* else, check if sum can be obtained by any
-of the following:
-	(a) including the last element
-	(b) excluding the last element */
-	return isSubsetSum(set, n - 1, sum)
-		|| isSubsetSum(set, n - 1, sum - set[n - 1]);
+       
+
+	    Solution ob;
+	    cout << ob.minDifference(a, n) << "\n";
+	     
+    }
+    return 0;
 }
-
-// Driver code
-int main()
-{
-	int set[] = { 3, 34, 4, 12, 5, 2 };
-	int sum = 9;
-	int n = sizeof(set) / sizeof(set[0]);
-	if (isSubsetSum(set, n, sum) == true)
-		cout <<"Found a subset with given sum";
-	else
-		cout <<"No subset with given sum";
-	return 0;
-}
-
-// This code is contributed by shivanisinghss2110
+// } Driver Code Ends
